ab3e8f6848f898466b86caddbe3373785fcd0d40
gabinoMSdev <gabino.ms.dev@gmail.com>
2025-01-24 13:26:38 +0100

    book getters and setters

--- Library/src/Book.java
+++ Library/src/Book.java
@@ -1,3 +1,46 @@
 public class Book {
+    private String title, author;
+    private Category category;
+    private Status status;
 
+    public Book() {
+        title = "Default";
+        author = "Default";
+        category = Category.Default;
+        status = Status.Available;
+    }
+
+    // getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    // setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
